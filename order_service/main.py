0c8121f854f16e1d496b491b05a14c9afcfa25a1
Garvita Jain <gjain2@umbc.edu>
2026-08-27 22:28:34 -0400

    Add POST endpoint for orders

--- order_service/main.py
+++ order_service/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class OrderItem(BaseModel):
+    product_id: int
+    quantity: int
+
+class Order(BaseModel):
+    customer_id: int
+    items: list[OrderItem]
 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+@app.post("/orders")
+def create_order(order: Order):
+    return {"Order Created for Customer Id": order.customer_id}
