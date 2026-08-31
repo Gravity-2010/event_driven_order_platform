695a90d04688e0f270daebbc3f2827a7fbe4f131
Garvita Jain <gjain2@umbc.edu>
2026-08-30 22:28:03 -0400

    Add create and get order functionality along with in-memory storage

--- order_service/main.py
+++ order_service/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
+order_dict = {}
+next_order_id = 1
 
 class OrderItem(BaseModel):
     product_id: int
@@ -18,4 +20,13 @@ def health_check():
 
 @app.post("/orders")
 def create_order(order: Order):
+    global next_order_id 
+    order_dict[next_order_id] = order
+    next_order_id += 1
     return {"Order Created for Customer Id": order.customer_id}
+
+@app.get("/orders/{order_id}")
+def get_order(order_id: int):
+    if order_id not in order_dict:
+        raise HTTPException(status_code=404, detail="Order not found")
+    return order_dict[order_id]
